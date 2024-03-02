0403d46483705a1772c83def5a4c341e779588c5
forknknife <147292669+forknknife@users.noreply.github.com>
2024-03-01 16:16:03 -0800

    Add files via upload

--- server.R
+++ server.R
@@ -1,26 +1,203 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(ggthemes)
+library(plotly)
 
-library(shiny)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+function(input, output) {
+
   
-})
+    
+  # P2 Table 2 (Similarity Index)
+  output$table <- renderTable({
+    data <- read.csv("data/Final_Data.csv")
+    data$season <- as.factor(data$season)
+    data$player <- as.factor(data$player)
+
+    p_data <- data %>% 
+        select(Category) %>% 
+        filter(data$player == input$bat)
+      p_data <- toString(unique(p_data[,1]))
+    
+      similar_player <- data %>% 
+      #select(player, Team, Category, Batting_Avg, MadMax_Mode) %>% 
+      filter(data$Category == p_data ) %>% 
+      group_by(player, Role, Team) %>%  
+      summarise("Batting Avg." = round(sum(Runs)/(sum(R25[gotout==1]) + sum(R50[gotout==1]) + sum(R75[gotout==1]) + sum(R100[gotout==1]))))
+
+    head(similar_player, 10)
+    
+  }, bordered = TRUE, hover = TRUE, striped = TRUE, caption = "(based on k-means algorithm)", rownames = TRUE, colnames = TRUE, width = "500px")
+  
+  ## Preview
+  output$preview <- renderTable({
+    data <- read.csv("data/Final_Data.csv")
+    data$season <- as.factor(data$season)
+    data$player <- as.factor(data$player)
+    
+    data <- data %>% 
+         filter(data$player == input$bat )
+    
+  
+    colnames(data)[colnames(data)=="player"] <- "Player"
+    colnames(data)[colnames(data)=="Category"] <- "SkillLevel"
+ 
+    dt <- data %>%
+      group_by(Player, SkillLevel,Role) %>%
+      summarise("Batting Avg." = round(sum(Runs)/(sum(R25[gotout==1]) + sum(R50[gotout==1]) + sum(R75[gotout==1]) + sum(R100[gotout==1]))),
+                "Runs Made" = sum(Runs),
+                "Hard Hit Ratio(%)" = round(sum(Big_Runs) / sum(Runs)),
+                "Bowling Avg." = round(sum(Runs_Given)/sum(Wickets)),
+                "Wickets Taken" = sum(Wickets),
+                "Runs_Given" = sum(Runs_Given),
+                "Runs made on 1st innings" = sum(Runs[inning==1]),
+                "Runs made on 2nd innings" = sum(Runs[inning==2]))
+    
+    dt <- as.data.frame(t(dt))
+    #colnames(dt)[colnames(dt)=="V1"] <- "F"
+    
+    
+  }, caption = "numbers based on IPL season 2008-2017", bordered = TRUE, hover = TRUE, striped = TRUE, rownames = TRUE, colnames = FALSE, width = "500px")
+  
+  output$text1 <- renderText({"Player Statistics"})
+  output$text2 <- renderText({"Similar Players"})
+  output$space1 <- renderText({"   "})
+  output$space2 <- renderText({"   "})
+  output$space3 <- renderText({"   "})
+  
+## Plot 1 the by city stats
+  output$main_plot <- renderPlot({
+    data <- read.csv("data/Final_Data.csv")
+    data <- data[data$player == input$bat,]
+    data <- 
+      data %>%
+      select(city, Runs, R25,R50, R75, R100, gotout) %>% 
+      filter(city  != "") %>% 
+      group_by(city) %>%
+      summarize(Batting_Avg_Num = sum(Runs),
+                Batting_Avg_Den = sum(R25[gotout==1]) + sum(R50[gotout==1]) + sum(R75[gotout==1]) + sum(R100[gotout==1]))
+      data <- data %>% 
+              group_by(city) %>% 
+              summarize(Batting_Avg = round(mean(Batting_Avg_Num/Batting_Avg_Den)),0)
+      
+      if(is.infinite(data$Batting_Avg) == TRUE)
+      data$Batting_Avg[is.infinite(data$Batting_Avg)]<-NA
+      data <- as.data.frame(data)
+      data <- na.omit(data)
+      
+      
+      gg <- ggplot(data, aes(reorder(city,Batting_Avg,mean),Batting_Avg, fill = "red", label = Batting_Avg) )
+      gg+geom_bar(stat = "identity" )+
+      geom_text(size = 3, position = position_stack(vjust = 0.5)) +
+      coord_flip()+
+      labs(title="Batting Performance (Avg.) by City") + 
+      theme_dark() + theme(axis.text.x = element_blank(),
+                                              axis.title.y=element_blank(),
+                                              axis.title.x=element_blank(),
+                                              axis.ticks = element_blank(),
+                                              legend.position="none")
+
+  })
+## Plot 2 the by year stats
+#   output$year_plot <- renderPlot({
+#       data <- read.csv("data/Final_Data.csv")
+#       data <- data[data$player == input$bat,]
+#       data <- 
+#         data %>%
+#         select(season, Runs, R25,R50, R75, R100, gotout) %>% 
+#         group_by(season) %>%
+#         summarize(Batting_Avg_Num = sum(Runs),
+#                   Batting_Avg_Den = sum(R25[gotout==1]) + sum(R50[gotout==1]) + sum(R75[gotout==1]) + sum(R100[gotout==1]))
+#       data <- data %>% 
+#         group_by(season) %>% 
+#         summarize(Batting_Avg = round(mean(Batting_Avg_Num/Batting_Avg_Den)),0)
+#       
+# 
+#     ggplot(data, aes(as.integer(season), Batting_Avg) ) + 
+#       labs(title="Batting Performance (Avg.) by Season") +
+#       theme_dark() +
+#       theme(legend.position="none",
+#             axis.text.y = element_blank(),
+#             axis.title.y=element_blank(),
+#             axis.title.x=element_blank(),
+#             axis.ticks = element_blank())
+#      
+#     
+#   })
+#   
+# # ## Page 1 Plot 1
+# #   ## Plot 1 the by city stats
+# #   output$team1 <- renderPlot({
+# #     data <- read.csv("data/batting.csv")
+#     data$Batsman <- as.factor(data$batsman)
+#     data <- data[data$city == input$city ,]
+#     data <- data[data$Team == input$team1 ,]
+#     
+#     
+#     data <- data %>%
+#       #select(Batsman,Role, SkillLevel,inning, NotOuts,  Bowling_Avg, Runs_Given, Wickets, Runs, MadMax_Mode) %>% 
+#       group_by(Batsman, city, Role, Category) %>%
+#       summarise(mean_batting = round(mean(sum(Runs/(sum(R25+R50+R75+R100) - sum(NotOuts)))))) %>% 
+#       arrange(desc(mean_batting))
+#      
+#     ggplot(data = data, aes(x = reorder(Batsman,mean_batting,sum), Batsman, y = mean_batting,  fill=Category) ) + 
+#       geom_bar(stat="identity", width = 0.5 )  +
+#       coord_flip()+ 
+#       scale_fill_brewer(palette="Set1")+
+#       labs(title="Batting Performance (Avg.)") +
+#       theme_hc(bgcolor = "darkunica") +
+#       ylim(0, 100)+
+#       theme(axis.text.x = element_blank(),
+#             axis.title.y=element_blank(),
+#             axis.title.x=element_blank(),
+#             axis.ticks = element_blank())
+#     
+#     
+#   })
+# 
+#   ## Page 1 Space
+# output$p1s1 <- renderText({"   "})
+#   
+#   ## Page 1 Plot 2
+#   ## Plot 1 the by city stats
+#   output$team2 <- renderPlot({
+#     data <- read.csv("data/batting.csv")
+#     data$Batsman <- as.factor(data$batsman)
+#     data <- data[data$city == input$city ,]
+#     data <- data[data$Team == input$team2 ,]
+#     
+#     
+#     data <- data %>%
+#       #select(Batsman,Role, SkillLevel,inning, NotOuts,  Bowling_Avg, Runs_Given, Wickets, Runs, MadMax_Mode) %>% 
+#       group_by(Batsman, city, Role, Category) %>%
+#       summarise(mean_batting = round(mean(sum(Runs/(sum(R25+R50+R75+R100) - sum(NotOuts)))))) %>% 
+#       arrange(desc(mean_batting))
+#     
+#     ggplot(data = data, aes(x = reorder(Batsman,mean_batting,sum), Batsman, y = mean_batting,  fill=Category) ) + 
+#       geom_bar(stat="identity", width = 0.5 )  +
+#       coord_flip()+ 
+#       scale_fill_brewer(palette="Set1")+
+#       labs(title="Batting Performance (Avg.)") +
+#       theme_hc(bgcolor = "darkunica") +
+#       ylim(0, 100)+
+#       theme(axis.text.x = element_blank(),
+#             axis.title.y=element_blank(),
+#             axis.title.x=element_blank(),
+#             axis.ticks = element_blank())
+#     
+#     
+#   })
+  
+
+  
+  
+  
+  
+
+  
+}
--- ui.R
+++ ui.R
@@ -1,32 +1,123 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
-batting <- read.csv("/Users/sachrath/Desktop/IPLFan/batting.csv")
-batsmen <- unique(as.list(batting$batsman))
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+library(ggplot2)
+library(shinythemes)
+#library(DT)
+
+batting <- read.csv("data/batting.csv")
+filters <- read.csv("data/FiltersTeam.csv")
+batsmen <- as.character(as.factor(unique(unlist(batting$batsman)))) 
+city <- as.character(as.factor(unique(unlist(batting$city)))) 
+team1 <- as.character(as.factor(unique(unlist(filters$Team))))
+team2 <- as.character(as.factor(unique(unlist(filters$Team))))
+role <- as.character(as.factor(unique(unlist(filters$Role))))
+
+
+fluidPage(align="center",
+  theme = shinytheme("slate"),
+  titlePanel(title=, "IPL Analytics"),
+  tags$head(tags$style(
+    HTML('
+         body, label, input, button, select { 
+         background-color: black    ;
+         }')
+    )),
+  tags$head(includeScript("google-analytics.js")),
+  # Create a new Row in the UI for selectInputs
+  navbarPage("", id="nav",
+  # tabPanel("Game Analyzer",
+  #          fluidRow(splitLayout(align="center", style="font-style: italic", height = "400px",cellWidths = c("500px","250px","500px"),
+  #            selectInput("team1","Select First Team",team1), 
+  #            selectInput("city","Select playing ground",city), 
+  #            selectInput("team2","Select Second Team",team2)
+  #          )),
+  #          
+  #          # Create a new row for the table.
+  #          
+  #         fluidRow(
+  #           splitLayout(align="center",cellWidths = c("500px","250px", "500px" ), 
+  #                       plotOutput(outputId = "team1", height = "400px"), 
+  #                       textOutput("p1s1"),
+  #                       plotOutput(outputId = "team2", height = "400px"))
+  #           )
+  #          
+  # ),
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("bat",
-                  "batsman:",
-                  batsmen)
+  tabPanel("Player Analyzer",
+  fluidRow(
+    selectInput("bat","Select a player",batsmen)
     ),
+
+  # Create a new row for the table.
+ 
+  fluidRow(splitLayout(align="center", style="font-style: italic", cellWidths = c("500px","50px","500px"),
+                       textOutput("text1"), 
+                       textOutput("space1"),
+                       textOutput("text2"))),
+  fluidRow(
+    splitLayout(align="center",cellWidths = c("500px","50px","500px" ), 
+                tableOutput("preview"), 
+                textOutput("space2"),
+                tableOutput("table"))),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+  fluidRow(
+  splitLayout(align="center",cellWidths = c("1000px" ), 
+              plotOutput(outputId = "main_plot", height = "400px")
+              # ,
+              # textOutput("space3"),
+              # plotOutput(outputId = "year_plot", height = "400px")
+              )
+)
+),
+tabPanel("About",
+         align="left",
+         h3("Welcome"),
+         p("If you have come to page from Facebook ads, then THANK YOU! it means a lot to me. This is a mini project I am working on IPL Analytics,
+           as a Data Scientist and sport fanatics I always wanted to do something like this. Now this is only a starting point, based on your feedback 
+           I will have a new version of this website. Why I am doing this! well, this year I played fantasy IPL for first time, and I loved it, I follwed 
+           analytics to make best team, and it trust me it works, data tells the story, so before next season kicks off I will have this ready, I promise, 
+           and with your help I will add new features, hence, please give your valuable opinion, peace."),
+         h3("About this website"),
+         p("Every year a summit of extraordinary players creating extravagant events. The money, time, competitions on this event clearly shows 
+            it’s not just a domestic occasion but a worldwide phenomenon. This league 
+           has another exciting aspect, i.e. statistics", em("(well, in short, cricket is all about it) "),
+           " and undoubtedly, has attracted larger crowed who actively participate in fantasy sports. This website is for those enthusiast who believes that data speaks for itself 
+           when combined with mathematical models, together it tells how, why, and who are the best picks among the large pool who can make it for 
+           fantasy team."),
+         
+         br(),
+         br(),
+         h3("About the Data and Measures"),
+         h4("The Data"),
+         p("I have been collecting this data since 2 years from various, it’s not my day job, so took a while shaping everything together,
+           however, the data was mostly reconciled with most of the publishers, but I cannot guarantee the accuracy of the numbers, the data
+           may not represent the actual records."),
+         h4("Basic Measures"),
+         p("Some of the measures are pretty straightforward, and directly referenced from www.wikipedia.com,such as 
+           batting/bowling average, bowler’s economy, total runs made."), 
+         h4("Derived Measures"),
+         p("Hard Hit Ratio(%): Ratio of total runs made in boundaries and sixes by total runs."),
+         p("Runs made on 1st innings: Runs made by batsmen in 1st innings."),
+         p("Runs made on 2nd innings: Runs made by batsmen in 2nd innings, this measure when compared with above tells us whether the 
+           batsman is a better attacker or chaser."),
+         br(),
+         br(),
+         h3("The analysis of Similar players"),
+         p("You might have observed that, when a player is selected, the table with similar players also gets updated, this is based on a
+           mathematical modeling method, known as K-Mean clustering. K-means clustering (each observation to the cluster whose mean 
+           has the least squared Euclidean distance, this is intuitively the nearest mean) is popular for cluster analysis in data mining.
+           you can read more about this method, here, https://en.wikipedia.org/wiki/K-means_clustering."),
+         br(),
+         br(),
+         h3("About Me"),
+         p("For an introduction, follow me on LinkedIn",
+           a("Amit Rath", 
+             href = "https://www.linkedin.com/in/amirath"))
+         
+         
+         
+) 
+)
+)
+
+
+
